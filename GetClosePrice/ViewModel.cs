1cf5fbb966faeb52d560e2e44fd1496f9901eb34
todomato <dreamrrrr@hotmail.com>
2017-08-11 10:22:01 +0800

    fix excel會有單引號問題

--- GetClosePrice/ViewModel.cs
+++ GetClosePrice/ViewModel.cs
@@ -8,38 +8,41 @@ namespace GetClosePrice
 {
     public class ViewModel
     {
-        private string[] item;
-
         public ViewModel(string[] item, string type)
         {
             if (type == "sii")
             {
-                this.Code = item[0];
+                this.Code = int.Parse(item[0]) ;
                 this.Name = item[1];
-                this.Volumn = item[2];
-                this.Open = item[5];
-                this.High = item[6];
-                this.Low = item[7];
-                this.Close = item[8];
+                this.Volumn = float.Parse(item[2]);
+                this.Open = IsNumeric(item[5]) ? float.Parse(item[5]) : 0;
+                this.High = IsNumeric(item[6]) ? float.Parse(item[6]) : 0;
+                this.Low = IsNumeric(item[7]) ? float.Parse(item[7]) : 0;
+                this.Close = IsNumeric(item[8]) ? float.Parse(item[8]) : 0;
             }
             else
             {
-                this.Code = item[0];
+                this.Code = int.Parse(item[0]);
                 this.Name = item[1];
-                this.Volumn = item[8];
-                this.Open = item[4];
-                this.High = item[5];
-                this.Low = item[6];
-                this.Close = item[2];
+                this.Volumn = float.Parse(item[8]);
+                this.Open = IsNumeric(item[4]) ? float.Parse(item[4]) : 0;
+                this.High = IsNumeric(item[5]) ? float.Parse(item[5]) : 0;
+                this.Low = IsNumeric(item[6]) ? float.Parse(item[6]) : 0;
+                this.Close = IsNumeric(item[2]) ? float.Parse(item[2]) : 0;
             }
         }
-        public string Code { get; set; }
+        public int Code { get; set; }
         public string Name { get; set; }
-        public string Close { get; set; }
-        public string Open { get; set; }
-        public string High { get; set; }
-        public string Low { get; set; }
-        public string Volumn { get; set; }
+        public float Close { get; set; }
+        public float Open { get; set; }
+        public float High { get; set; }
+        public float Low { get; set; }
+        public float Volumn { get; set; }
 
+        private static bool IsNumeric(string s)
+        {
+            float output;
+            return float.TryParse(s, out output);
+        }
     }
 }
